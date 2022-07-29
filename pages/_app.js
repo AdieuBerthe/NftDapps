b5c031fad85094be00002069a7672e8fa75857e2
adnane-t <adnane.t@gmail.com>
2022-07-29 15:42:31 +0200

    update menu with active class to know where we are

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,66 @@
-import { UserContextProvider } from '../context/context';
-import '../styles/globals.css'
-import Link from 'next/link'
+import { UserContextProvider } from "../context/context";
+import "../styles/globals.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <div>
-      <nav className="border-b p-6 text-center" >
+      <nav className="border-b p-6 text-center">
         <p className="text-4xl font-bold">NFT Marketplace</p>
         <div className="flex justify-center mt-4">
           <Link href="/">
-            <a className="mr-4 text-blue-800">
+            <a
+              className={
+                router.pathname === "/"
+                  ? "mr-4 text-blue-800 underline underline-offset-8 decoration-6"
+                  : "mr-4 text-blue-800"
+              }
+            >
               Home
             </a>
           </Link>
           <Link href="/create-nft">
-            <a className="mr-6 text-blue-800">
+            <a
+              className={
+                router.pathname === "/create-nft"
+                  ? "mr-6 text-blue-800 underline underline-offset-8 decoration-6"
+                  : "mr-6 text-blue-800"
+              }
+            >
               Create NFT
             </a>
           </Link>
           <Link href="/collection">
-            <a className="mr-6 text-blue-800">
+            <a
+              className={
+                router.pathname === "/collection"
+                  ? "mr-6 text-blue-800 underline underline-offset-8 decoration-6"
+                  : "mr-6 text-blue-800"
+              }
+            >
               Collections
             </a>
           </Link>
           <Link href="/my-nfts">
-            <a className="mr-6 text-blue-800">
+            <a
+              className={
+                router.pathname === "/my-nfts"
+                  ? "mr-6 text-blue-800 underline underline-offset-8 decoration-6"
+                  : "mr-6 text-blue-800"
+              }
+            >
               My NFTs
             </a>
           </Link>
-          </div>
+        </div>
       </nav>
       <UserContextProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </UserContextProvider>
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
